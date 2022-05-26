2eb644afe360bd6f64b444f2f31c2918d72b6357
NicolasIceberg <93177243+MykolaKovalyk@users.noreply.github.com>
2022-05-26 11:24:52 +0300

    Better code readability for number replacer

--- src/main/java/com/lpnu/iot/term2/lab7/StringNegativeNumberReplacer.java
+++ src/main/java/com/lpnu/iot/term2/lab7/StringNegativeNumberReplacer.java
@@ -11,8 +11,8 @@ public class StringNegativeNumberReplacer {
     public String getModifiedString(String original) {
         String modified = original;
 
-        var pattern =  Pattern.compile("-\\d+");
-        var matcher  =  pattern.matcher(original);
+        var pattern = Pattern.compile("-\\d+");
+        var matcher = pattern.matcher(original);
 
         // needed because positive numbers are 1 character smaller than negative
         int offset = 0;
@@ -22,12 +22,12 @@ public class StringNegativeNumberReplacer {
             int end = matcher.end();
 
             var replaced = modified.substring(start - offset + 1, end - offset);
-            replaced =  new StringBuffer(replaced).reverse().toString();
+            replaced = new StringBuffer(replaced).reverse().toString();
 
-            modified =
-                      modified.substring(0, start -  offset)
-                    + replaced
-                    + modified.substring(end - offset, modified.length());
+            modified = new StringBuffer(modified)
+                    .replace(start - offset, end - offset, replaced)
+                    .toString();
+
 
             offset++;
         }
